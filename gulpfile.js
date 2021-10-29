19d0c42db6749126757f3db21f6504ac4c9c4cce
Eric Betts <bettse@fastmail.fm>
2021-10-29 08:28:40 -0700

    Get charCryptoExample working with node v16.2.0

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,10 @@ const transform = require('vinyl-transform');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 
-const browserifyOpts = {
-	entries: ['./browser.js'],
-	insertGlobals : true,
-	debug : false,
-	standalone: 'ld',
-	transforms: [
-		babelify.configure({
-			// Use all of the ES2015 spec
-			presets: ["es2015"]
-		})
-	]
-}
-
-gulp.task('default', ['node','browser'])
-gulp.task('node', ['es6'])
-gulp.task('browser', ['bundle']);
-
-gulp.task('es6', ()=>{
+gulp.task('default', ()=>{
 	return gulp.src('src/**/*.js')
 		.pipe(babel({
 			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('./dist'));
 });
-
-gulp.task('bundle', ['es6'], ()=>{
-	return browserify(browserifyOpts)
-  		.bundle()
-  		.pipe(source('./browser.js'))
-  		.pipe(buffer())
-  		.pipe(rename({ basename: 'node-ld' }))
-  		.pipe(gulp.dest('./build/browser/'))
-  		.pipe(uglify())
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('./build/browser/'));
-})
